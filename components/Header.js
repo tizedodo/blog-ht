93d2c8cb0117e2fea1c38c9a19ff2d333225fcdb
tize <103060616+tizedodo@users.noreply.github.com>
2024-04-26 05:26:53 -0500

    Actualizar Header.js

--- components/Header.js
+++ components/Header.js
@@ -2,13 +2,25 @@ import Link from 'next/link';
 
 export default function Header({ name }) {
   return (
-    <header className="pt-20 pb-12">
-      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
-      <p className="text-2xl dark:text-white text-center">
+    <header className="flex justify-between items-center pt-20 pb-12">
+      <div>
         <Link href="/">
-          {name}
+          <a>
+            <img src="/logo.png" alt="Logo" className="w-12 h-12 rounded-full bg-gradient-conic from-gradient-3 to-gradient-4" />
+          </a>
         </Link>
-      </p>
+      </div>
+      <div className="text-2xl dark:text-white">
+        <Link href="/">
+          <a className="mr-4">Home</a>
+        </Link>
+        <Link href="/about">
+          <a className="mr-4">About</a>
+        </Link>
+        <Link href="/contact">
+          <a>Contact</a>
+        </Link>
+      </div>
     </header>
   );
 }
